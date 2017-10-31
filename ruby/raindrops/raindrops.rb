2d3d050337b2fb99eb3ae35b4d7291659d0bd151
Luiz Neiva <luizgzn@gmail.com>
2017-10-31 00:36:25 -0200

    Factorizing and selecting values done

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,6 +1,10 @@
+require 'prime'
+
 module BookKeeping; VERSION = 3; __END__
 class Raindrops
-    def convert(number)
+    def self.convert(number)
+      factors = number.prime_division.map(&:first)
+      if factors.any? { |number| [3, 5, 7].include? number }
         
     end
 end
