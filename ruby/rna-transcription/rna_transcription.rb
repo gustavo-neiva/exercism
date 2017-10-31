95195f035948461b62a07f6b2780b6408e439e2f
Luiz Neiva <luizgzn@gmail.com>
2017-10-30 23:24:20 -0200

    Refactored

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -2,6 +2,6 @@ module BookKeeping; VERSION = 4; end
 class Complement
     def self.of_dna(sequence)
         return '' unless sequence =~ /\A[GCTA]+\z/
-        sequence.gsub!(/[CATG]/, "T"=> "A", "A"=> "U", "C"=> "G", "G"=> "C")
+        sequence.tr("GATC", "CUAG")
     end
 end
