7aa39274d50ed0e513be274b05a6f1510c51b543
Luiz Neiva <luizgzn@gmail.com>
2017-11-01 14:43:27 -0200

    Refactored sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -3,21 +3,21 @@ module BookKeeping; VERSION = 1; end
 class Sieve
   def initialize(number)
     if number >= 2 
-        @primes = [*2..number]
+      @primes = [*2..number]
     else
-        @primes = []
+      @primes = []
     end
   end
 
   def primes
     index = 0
     if @primes.length == 0
-        return []
+      return []
     else
       while @primes[index]**2 <= @primes.last
-          prime = @primes[index]
-          @primes = @primes.select { |num| num == prime || num%prime != 0 }
-          index += 1
+        prime = @primes[index]
+        @primes = @primes.select { |num| num == prime || num%prime != 0 }
+        index += 1
       end
     @primes
     end
