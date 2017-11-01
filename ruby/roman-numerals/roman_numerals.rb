cf24cc9c29508fa0458f55a76badb72824254b7e
Luiz Neiva <luizgzn@gmail.com>
2017-11-01 21:54:52 -0200

    Global variable and monkey patching

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,23 @@
+module BookKeeping; VERSION = 2; end
+
+class Fixnum
+    RN = {
+      "M": 1000,
+      "CM": 900,
+      "D": 500,
+      "CD": 400,
+      "C": 100,
+      "XC": 90,
+      "L": 50,
+      "XL": 40,
+      "X": 10,
+      "IX": 9,
+      "V": 5,
+      "IV": 4,
+      "I": 1
+    }
+    
+  def to_roman
+    
+  end 
+end
