df7022f671cb7e89876e3a7743cec5df0710bf79
Luiz Neiva <luizgzn@gmail.com>
2017-11-06 01:38:05 -0200

    Roman numerals encoder

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,23 +1,33 @@
 module BookKeeping; VERSION = 2; end
 
 class Fixnum
-    RN = {
-      "M": 1000,
-      "CM": 900,
-      "D": 500,
-      "CD": 400,
-      "C": 100,
-      "XC": 90,
-      "L": 50,
-      "XL": 40,
-      "X": 10,
-      "IX": 9,
-      "V": 5,
-      "IV": 4,
-      "I": 1
-    }
-    
+  
+  RN = {
+      1 => "I",
+      4 => "IV",
+      5 => "V",
+      9 => "IX",
+      10 => "X",
+      40 => "XL",
+      50 => "L",
+      90 => "XC",
+      100 => "C",
+      400 => "CD",
+      500 => "D",
+      900 => "CM",
+      1000 => "M"
+      }
+      
   def to_roman
-    
-  end 
+    result = ''
+    number = self
+    RN.keys.reverse.each do |decimal|
+      while number >= decimal
+        number -= decimal
+        result += RN[decimal]
+      end
+    end
+    result
+  end
+  
 end
