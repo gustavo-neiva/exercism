7a7e843e2e0261122bcf27658cb5db76383d41a7
Luiz Neiva <luizgzn@gmail.com>
2017-11-10 16:31:55 -0200

    leap year solved

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -2,10 +2,8 @@ module BookKeeping; VERSION = 3; end;
 
 class Year
   def self.leap?(year)
-    while year % 4 == 0 
-       unless year % 100 == 0 || year % 400 == 0
-        true
-       end
-    end
+    if year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 )
+      true 
+    end 
   end
 end
