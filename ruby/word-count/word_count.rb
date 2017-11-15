7b0b3e847c0432a28ffbd551f644826555d77e56
Luiz Neiva <luizgzn@gmail.com>
2017-11-15 11:42:34 -0200

    Most of the words are being counted, just the quoted need to be dealed with

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,12 @@
+class Phrase
+  def initialize(string)
+    @string = string
+  end
+
+  def word_count
+    split = @string.downcase.split(/[^'\p{N}\p{L}\p{M}]+/)
+    split.each_with_object(Hash.new(0)){|key,hash| hash[key] += 1}
+  end
+end
+
+module BookKeeping; VERSION = 1; end;
