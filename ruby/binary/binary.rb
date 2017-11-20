c881069b226cff264f4e1c552d8ca6bc30cddd12
Luiz Neiva <luizgzn@gmail.com>
2017-11-20 13:59:10 -0200

    Refactored with reduce method

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -2,15 +2,7 @@
 
   def self.to_decimal(binary)
     if binary.delete("01") == ""
-      digits = (binary.length)-1
-      base = 2
-      decimal = 0
-      result = 0
-      binary.split("").each do |digit|
-        result += digit.to_i * (base**digits)
-        digits -= 1
-      end
-    result
+      binary.chars.reduce(0) { |result, element| (result*2) + element.to_i}
     else
       raise ArgumentError
     end
