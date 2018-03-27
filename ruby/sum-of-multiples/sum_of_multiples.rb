d181e19a7a3438becc5f6654eb9d2da98b50b636
luizgzn <luizgzn@gmail.com>
2018-03-27 19:16:00 -0300

    Solves sum of multiples

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,11 +1,17 @@
 class SumOfMultiples
-  def initialize(number1, number2)
-    @number1 = number1
-    @number2 = number2
+  def initialize(*number)
+    @numbers = number
   end
-
+  
   def to(upto)
-    upto
+    if @numbers.empty?
+      return 0
+    else
+      @numbers.map do |number|
+        (0..upto - 1).select { |char| (char % number).zero? }
+      end.flatten.uniq.inject(:+)
+    end
   end
-
 end
+
+module BookKeeping; VERSION = 2; end
--- ruby/sum-of-multiples/sum_of_multiples_test.rb
+++ ruby/sum-of-multiples/sum_of_multiples_test.rb
@@ -9,62 +9,62 @@ class SumOfMultiplesTest < Minitest::Test
   end
 
   def test_multiples_of_3_or_5_up_to_4
-    skip
+    #skip
     assert_equal 3, SumOfMultiples.new(3, 5).to(4)
   end
 
   def test_multiples_of_3_up_to_7
-    skip
+    #skip
     assert_equal 9, SumOfMultiples.new(3).to(7)
   end
 
   def test_multiples_of_3_or_5_up_to_10
-    skip
+    #skip
     assert_equal 23, SumOfMultiples.new(3, 5).to(10)
   end
 
   def test_multiples_of_3_or_5_up_to_100
-    skip
+    #skip
     assert_equal 2_318, SumOfMultiples.new(3, 5).to(100)
   end
 
   def test_multiples_of_3_or_5_up_to_1000
-    skip
+    #skip
     assert_equal 233_168, SumOfMultiples.new(3, 5).to(1000)
   end
 
   def test_multiples_of_7_13_or_17_up_to_20
-    skip
+    #skip
     assert_equal 51, SumOfMultiples.new(7, 13, 17).to(20)
   end
 
   def test_multiples_of_4_or_6_up_to_15
-    skip
+    #skip
     assert_equal 30, SumOfMultiples.new(4, 6).to(15)
   end
 
   def test_multiples_of_5_6_or_8_up_to_150
-    skip
+    #skip
     assert_equal 4_419, SumOfMultiples.new(5, 6, 8).to(150)
   end
 
   def test_multiples_of_5_or_25_up_to_51
-    skip
+    #skip
     assert_equal 275, SumOfMultiples.new(5, 25).to(51)
   end
 
   def test_multiples_of_43_or_47_up_to_10000
-    skip
+    #skip
     assert_equal 2_203_160, SumOfMultiples.new(43, 47).to(10000)
   end
 
   def test_multiples_of_1_up_to_100
-    skip
+    #skip
     assert_equal 4_950, SumOfMultiples.new(1).to(100)
   end
 
   def test_multiples_of_an_empty_list_up_to_10000
-    skip
+    #skip
     assert_equal 0, SumOfMultiples.new().to(10000)
   end
 
@@ -86,7 +86,7 @@ class SumOfMultiplesTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    #skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
