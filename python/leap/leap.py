0500d56eb17812dcd95d8d507100c23741001391
Luiz Neiva <luizgzn@gmail.com>
2018-09-17 14:43:25 -0300

    Simplify code

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,5 +1,2 @@
 def is_leap_year(year):
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        return True
-    else:
-        return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
