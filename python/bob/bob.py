fa696c30854f675dee51b6fc2221b3e2bc52d077
Luiz Neiva <luizgzn@gmail.com>
2018-09-25 17:48:14 -0300

    Second iteration bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,11 +2,11 @@ def hey(phrase):
     phrase = phrase.strip()
     if not phrase:
         return "Fine. Be that way!"
-    elif phrase.endswith("?") and phrase.isupper():
-        return "Calm down, I know what I'm doing!"
-    elif phrase.endswith("?") and not phrase.isupper():
-        return "Sure."    
-    elif phrase.isupper():
+    if phrase.endswith("?"):
+        if phrase.isupper():
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Sure."    
+    if phrase.isupper():
         return "Whoa, chill out!"
-    else:
-        return "Whatever."
+    return "Whatever."
