649bc9ae1c56d89eb7c2d9b213cd9e3c063bf401
Gustavo Neiva <luizgzn@gmail.com>
2024-08-04 13:55:31 -0300

    solve bob exercism for real

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -4,17 +4,19 @@ defmodule Bob do
     upcase? = fn x -> x == String.upcase(x) end
 
     cond do
-      String.ends_with?(input, "?") ->
-        "Sure."
+      input |> String.replace(" ", "") |> String.trim() |> String.length() |> Kernel.==(0) ->
+        "Fine. Be that way!"
 
-      upcase?.(input) ->
-        "Whoa, chill out!"
-
-      upcase?.(input) and String.ends_with?(input, "?") ->
+      upcase?.(input) and String.ends_with?(input, "?") and Regex.match?(~r/[A-Z]/, input) ->
         "Calm down, I know what I'm doing!"
 
-      String.length(input) == 0 ->
-        "Fine. Be that way!"
+      upcase?.(input) and Regex.match?(~r/\p{Lu}/u, input) ->
+        "Whoa, chill out!"
+
+      input
+      |> String.replace(" ", "")
+      |> (fn transformed_input -> String.ends_with?(transformed_input, "?") end).() ->
+        "Sure."
 
       true ->
         "Whatever."
