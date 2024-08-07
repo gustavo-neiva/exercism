b0e23fee432fc152cae204ccfa9eae01bd88c7d8
Gustavo Neiva <luizgzn@gmail.com>
2024-08-06 23:32:57 -0300

    solve freelancer-rates

---
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,17 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount - before_discount * discount / 100
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    trunc(apply_discount(daily_rate(hourly_rate) * 22, discount) |> Float.ceil())
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    (budget / apply_discount(daily_rate(hourly_rate), discount)) |> Float.floor(1)
+  end
+end
